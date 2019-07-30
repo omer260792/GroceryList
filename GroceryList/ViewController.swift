cccb8cfce5e02b08f263a34b4593e13cc38e9aa8
Omer Cohen <omer@msapps.mobi>
2019-07-30 18:11:11 +0300

    Initial Commit

---
+++ GroceryList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroceryList
+//
+//  Created by Omer Cohen on 7/30/19.
+//  Copyright © 2019 Omer Cohen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
